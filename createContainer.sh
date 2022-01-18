d014f6c4c9d6d7dd7882614e55bf0cb5597b098d
Scott Aaron <dr_saaron@yahoo.com>
2022-01-18 09:35:04 -0600

    version update

---
+++ createContainer.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+containerName=mongodb
+imageName=mongo
+
+force=1
+if [ -n "$force" ] # pullLatestDocker.sh -i $imageName
+then
+    echo "re-building container $containerName"
+
+    docker stop $containerName
+    docker rm $containerName
+
+    # tag image with today's date
+    today=$(date '+%Y%m%d')
+    docker tag $imageName:latest $imageName:$today
+
+    # create data directory
+    [ -d data ] || mkdir data
+
+    # start 'er up
+    docker run --name $containerName -v `pwd`/data:/data/db -p 27017:27017 --health-cmd="echo 'db.runCommand(\"ping\").ok' | mongosh localhost:27017/test --quiet" -d --user $(id -u):$(id -g) $imageName:$today
+else
+    echo "no new image so nothing to build"
+fi
---
+++ node_test/index.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+      await client.connect();
+
+      // use my pyTest database, created by the python script above'
+      const database = client.db('pyTest');
+      const reviews = database.collection('reviews');
+
+      // find some mexican food
+      const query = { cuisine: "Mexican" };
+      const cuisine = await reviews.findOne(query);
+      console.log(cuisine);
+  } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+  }
+}
+
+run().catch(console.dir);
