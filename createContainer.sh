d102f6f732b7e8d76e08583f9c16936c0aaf6581
Scott Aaron <dr_saaron@yahoo.com>
2022-04-12 16:52:10 -0500

    add a couple local files/directories to make mongosh work

--- createContainer.sh
+++ createContainer.sh
@@ -3,7 +3,19 @@
 containerName=mongodb
 imageName=mongo
 
-if pullLatestDocker.sh -i $imageName
+while getopts :f OPTION
+do
+    case $OPTION in
+	f)
+	    force=1
+	    ;;
+	*)
+	    echo "invalid option $OPTARG" 1>&2
+	    exit 1
+    esac
+done
+
+if [ -n "$force" ] || pullLatestDocker.sh -i $imageName
 then
     echo "re-building container $containerName"
 
@@ -16,9 +28,11 @@ then
 
     # create data directory
     [ -d data ] || mkdir data
+    [ -d .mongodb ] || mkdir .mongodb
+    [ -f .dbshell ] || touch .dbshell
 
     # start 'er up
-    docker run --name $containerName -v `pwd`/data:/data/db -p 27017:27017 --health-cmd="echo 'db.runCommand(\"ping\").ok' | mongo localhost:27017/test --quiet" -d --user $(id -u):$(id -g) $imageName:$today
+    docker run --name $containerName -v `pwd`/data:/data/db -v `pwd`/.mongodb:/.mongodb -v `pwd`/.dbshell:/.dbshell -p 27017:27017 --health-cmd="echo 'db.runCommand(\"ping\").ok' | mongosh localhost:27017/test --quiet" -d --user $(id -u):$(id -g) $imageName:$today
 else
     echo "no new image so nothing to build"
 fi
